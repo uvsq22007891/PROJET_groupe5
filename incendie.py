158b40f8b6ca63b1e4646fb544cc1fbca8c43e80
Antony MONCLER <prof.antony@hotmail.fr>
2021-03-11 14:42:23 +0100

    new

--- incendie.py
+++ incendie.py
@@ -4,7 +4,6 @@
 # Michel ALAND
 # Claude LOUEMBET
 # Samy
-<<<<<<< HEAD
 # Ruben 
 #######################################################################################################
 
@@ -12,13 +11,11 @@
 
 def charge (event):
     pass
-=======
+
 # Ruben AMOYAL
 #######################################################################################################
-<<<<<<< HEAD
->>>>>>> db274b7485dbaa1e213e8ed951d538ec57e5b11d
-=======
+
 def random_parcelle(event):
     """ me les parcelles aléatoirement sur le terrain """
     pass
->>>>>>> 2dcc41e672482a3d31e74b32fc373d57a98d2364
+
