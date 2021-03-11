778ecdf4dc3b3c736b92adf3aa9ad5632e71e09b
Antony MONCLER <prof.antony@hotmail.fr>
2021-03-11 14:44:53 +0100

    again and again

--- incendie.py
+++ incendie.py
@@ -7,6 +7,14 @@
 # Ruben 
 #######################################################################################################
 
+import tkinter as tk
+import random as rd 
+
+HEIGHT = 500
+WIDTH = 500
+largeur_case = WIDTH // 10
+hauteur_case = HEIGHT // 10
+#######################################################################################################
 """Cette fonction permet de charger une suavegarde de lasimulation des parcelles"""
 
 def charge (event):
@@ -17,5 +25,29 @@ def charge (event):
 
 def random_parcelle(event):
     """ me les parcelles aléatoirement sur le terrain """
-    pass
+    canvas = tk.Canvas(racine, bg="white", height=HEIGHT, width=WIDTH)
+    canvas.grid()
+    for i in range(10):
+        for j in range(10):
+            couleur=rd.randint(0,6)
+            if couleur == 0:
+                color="blue"
+            if couleur == 1:
+                color="green"
+            if couleur == 2:
+                color="red"
+            if couleur == 3:
+                color="yellow"
+            if couleur == 4:
+                color="grey"
+            if couleur == 5:
+                color="black"
+            canvas.create_rectangle((i*largeur_case, j*hauteur_case),
+                ((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
+#######################################################################################################
 
+
+
+racine = tk.Tk() # Création de la fenêtre racine
+
+racine.mainloop() # Lancement de la boucle principale
